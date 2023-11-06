cba5169a27094f750c1d7c7ebac65633a0729d6e
PasqualeIannace <pasquale.iannace@gmail.com>
2023-11-06 17:55:14 +0100

    Completato secondo esercizio

--- js/main.js
+++ js/main.js
@@ -18,3 +18,45 @@ function palindromo() {
         console.log(`La parola: ${input} è diversa da: ${inverso}. Non è palindroma!`)
     }
 }
+
+// PARI O DISPARI
+
+
+function pari() {
+    player = "pari";
+    return play();
+}
+
+function dispari() {
+    player = "dispari";
+    return play();
+}
+
+function play() {
+    const player = parseInt( document.getElementById("myNumber").value );
+    const pc = Math.floor(Math.random() * 5) + 1;
+
+    console.log("Player: ", player, " PC: ", pc);
+    return pariOrDispari(player, pc);
+}
+
+function pariOrDispari(player, pc) {
+
+    const total = player + pc;
+    console.log(total);
+    if(total % 2 == 0) {
+        return result("pari");
+    } else {
+        return result("dispari");
+    }
+}
+
+function result(risultato) {
+    console.log("Il risultato è: ", risultato, "player sceglie: ", player);
+
+    if(risultato == player) {
+        console.log("Hai vinto!")
+    } else {
+        console.log("Hai perso!")
+    }
+}
