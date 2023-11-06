325e26b93143e04f15156a8f4f4f261980851fae
PasqualeIannace <pasquale.iannace@gmail.com>
2023-11-06 17:17:03 +0100

    Completato primo esercizio

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 document.getElementById("paliBtn").addEventListener("click", function() {
+    return palindromo();
+})
+
+function palindromo() {
     input = document.getElementById("paliCheck").value;
     console.log(input);
 
-    let inverso;
+    let inverso = "";
 
-    for(i = input.length; i >= 0; i--) {
+    for(i = input.length - 1; i >= 0; i--) {
         inverso += input[i];
-        console.log(inverso);
     }
 
-    console.log(inverso);
-})
+    if(inverso == input) {
+        console.log("Questa parola è palindroma!");
+    } else {
+        console.log(`La parola: ${input} è diversa da: ${inverso}. Non è palindroma!`)
+    }
+}
